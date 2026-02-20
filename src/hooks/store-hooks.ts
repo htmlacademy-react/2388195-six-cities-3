de58d1d58d5e8733f49ee7c913c8839e68634a8c
Irina <i.ilchenko.spb@gmail.com>
2026-02-20 22:10:26 +0300

    исправлен useAppDispatch

--- src/hooks/store-hooks.ts
+++ src/hooks/store-hooks.ts
@@ -2,6 +2,6 @@ import {TypedUseSelectorHook, useDispatch, useSelector, useStore} from 'react-re
 import {RootState, AppDispatch} from '../types/store';
 import {store} from '../store';
 
-export const useAppDispatch = () => useDispatch<AppDispatch>();
+export const useAppDispatch: () => AppDispatch = useDispatch;
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
 export const useAppStore: ()=> typeof store = useStore;
