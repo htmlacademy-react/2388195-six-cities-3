175d29e3c4616d90623c016ef2d9ecafdddc2e21
Irina <i.ilchenko.spb@gmail.com>
2026-02-27 02:10:09 +0300

    Добавим поддержку токенов.
    Обновим модуль,
    где происходит конфигурирование axios. Добавим извлечение токена из
    localStorage и вставим его в заголовки. Для этого нам потребуется
    воспользоваться перехватчиками, а именно перехватчиком запроса.
    
    Настраиваем перехватчик, который будет вызывать формирования запроса,
    но до его отправки серверу. Здесь мы сможем модифицировать
    конфигурацию axios.

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,4 +1,5 @@
-import axios, {AxiosInstance} from 'axios';
+import axios, {AxiosInstance, InternalAxiosRequestConfig} from 'axios';
+import {getToken} from './token';
 import { BACKEND_URL, REQUEST_TIMEOUT } from '../const';
 
 export const createAPI = (): AxiosInstance => {
@@ -7,5 +8,23 @@ export const createAPI = (): AxiosInstance => {
     timeout: REQUEST_TIMEOUT,
   });
 
+  api.interceptors.request.use(
+    (config: InternalAxiosRequestConfig) => {
+      const token = getToken();
+
+      if (token && config.headers) {
+        config.headers['x-token'] = token;
+      }
+
+      return config;
+    },
+  );
+
   return api;
 };
+
+
+// В современных версиях axios (начиная с 1.x) использование типа
+// AxiosRequestConfig внутри перехватчика запросов приводит к ошибке типизации,
+// так как метод use ожидает тип InternalAxiosRequestConfig.
+// Этот тип гарантирует, что объект конфигурации (включая заголовки) уже инициализирован.
