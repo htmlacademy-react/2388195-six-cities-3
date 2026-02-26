95c30cfd0d90138be382c751ce968f9186d19031
Irina <i.ilchenko.spb@gmail.com>
2026-02-27 01:53:23 +0300

    Добавит модуль для работы с токеном.

--- src/const.ts
+++ src/const.ts
@@ -1,5 +1,6 @@
 export const BACKEND_URL = 'https://15.design.htmlacademy.pro/six-cities';
 export const REQUEST_TIMEOUT = 5000;
+export const AUTH_TOKEN_KEY_NAME = 'six-cities-token';
 
 export enum AppRoute {
   Root = '/',
