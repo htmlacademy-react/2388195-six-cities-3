f6af48bf75033734642ea73577e3cc1d6708a131
Irina <i.ilchenko.spb@gmail.com>
2026-02-27 01:41:32 +0300

    redux-thunk.
    Cконфигурируем хранилище. Подключим redux-thunk в список middlewares.
    Аргументом для thunk передадим сконфигурированный экземпляр axios,
    чтобы была возможность обратиться к нему из действия.

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,9 +1,17 @@
 import {configureStore} from '@reduxjs/toolkit';
 import { offersSlice } from './slices/offers-slice';
+import {createAPI} from '../services/api';
 
+export const api = createAPI();
 
 export const store = configureStore({
   reducer: {
     [offersSlice.name]: offersSlice.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      thunk: {
+        extraArgument: api,
+      },
+    }),
 });
