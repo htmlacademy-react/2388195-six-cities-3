17a3176b8ce6179ae74b80835ea836cff43d30d6
Irina <i.ilchenko.spb@gmail.com>
2026-03-02 01:38:48 +0300

    комменты для меня

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,11 +2,21 @@ import axios, {AxiosInstance, InternalAxiosRequestConfig} from 'axios';
 import {getToken} from './token';
 import { BACKEND_URL, REQUEST_TIMEOUT } from '../const';
 
+
+//сделать через переменную окружения
 export const createAPI = (): AxiosInstance => {
   const api = axios.create({
     baseURL: BACKEND_URL,
     timeout: REQUEST_TIMEOUT,
   });
+
+
+  //Перехватчики. Когда мы будем отправлять запросы...
+  // некоторые из этих запросов требуют заголовка с содержимым токена,
+  // т.е это запрос на добавление в избранное, и проч которые требуют токен для идентификации пользователя.
+  //Чтобы это не делать внутри каждого запроса, мы это делаем сразу для всех запросов
+  //т.е. мы перехватываем все запросы и подключаем к заголовку токен. Токен забираем с помощью функции getToken()
+
 
   api.interceptors.request.use(
     (config: InternalAxiosRequestConfig) => {
