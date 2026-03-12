8bd28cf3ebd7dc78890588f39567c417044541d4
Irina <i.ilchenko.spb@gmail.com>
2026-03-12 16:48:28 +0300

    закомментированы моки

--- src/mocks/comments.ts
+++ src/mocks/comments.ts
@@ -1,24 +1,24 @@
-export const COMMENTS = [
-  {
-    'id': 'b67ddfd5-b953-4a30-8c8d-bd083cd6b62a',
-    'date': '2019-05-08T14:13:56.569Z',
-    'user': {
-      'name': 'Oliver Conner',
-      'avatarUrl': './public/img/avatar-angelina.jpg',
-      'isPro': false
-    },
-    'comment': 'A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam.',
-    'rating': 4
-  },
-  {
-    'id': 'b67ddfd5-b953-4a30-8c8d-bd083cd6b63a',
-    'date': '2019-05-08T14:13:56.569Z',
-    'user': {
-      'name': 'Max Dewinter',
-      'avatarUrl': './public/img/avatar-max.jpg',
-      'isPro': true
-    },
-    'comment': 'Super.',
-    'rating': 5
-  },
-];
+// export const COMMENTS = [
+//   {
+//     'id': 'b67ddfd5-b953-4a30-8c8d-bd083cd6b62a',
+//     'date': '2019-05-08T14:13:56.569Z',
+//     'user': {
+//       'name': 'Oliver Conner',
+//       'avatarUrl': './public/img/avatar-angelina.jpg',
+//       'isPro': false
+//     },
+//     'comment': 'A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam.',
+//     'rating': 4
+//   },
+//   {
+//     'id': 'b67ddfd5-b953-4a30-8c8d-bd083cd6b63a',
+//     'date': '2019-05-08T14:13:56.569Z',
+//     'user': {
+//       'name': 'Max Dewinter',
+//       'avatarUrl': './public/img/avatar-max.jpg',
+//       'isPro': true
+//     },
+//     'comment': 'Super.',
+//     'rating': 5
+//   },
+// ];
--- src/mocks/offers.ts
+++ src/mocks/offers.ts
@@ -1,152 +1,152 @@
-import { FullOffers } from '../types/offer';
+// import { FullOffers } from '../types/offer';
 
 
-export const OFFERS: FullOffers = [{
-  'id': 'fbb92630-ceb0-4ddf-a594-a4ae39ecaa91',
-  'title': 'Beautiful & luxurious studio at great location',
-  'type': 'apartment',
-  'price': 120,
-  'city': {
-    'name': 'Paris',
-    'location': {
-      'latitude': 52.35514938496378,
-      'longitude': 4.673877537499948,
-      'zoom': 8
-    }
-  },
-  'location': {
-    'latitude': 52.35514938496378,
-    'longitude': 4.673877537499948,
-    'zoom': 8
-  },
-  'isFavorite': true,
-  'isPremium': false,
-  'rating': 5,
-  'description': 'A quiet cozy and picturesque that hides behind a a river by the unique lightness of Paris.',
-  'bedrooms': 3,
-  'goods': [
-    'Heating, Towels'
-  ],
-  'host': {
-    'name': 'Oliver Conner',
-    'avatarUrl': 'img/avatar-angelina.jpg',
-    'isPro': true
-  },
-  'images': [
-    'img/apartment-01.jpg, img/apartment-03.jpg'
-  ],
-  'maxAdults': 4,
-  previewImage: ''
-},
-{
-  'id': '2',
-  'title': 'Beautiful & luxurious studio at great location',
-  'type': 'apartment',
-  'price': 120,
-  'city': {
-    'name': 'Paris',
-    'location': {
-      'latitude': 52.35514938496378,
-      'longitude': 4.673877537499948,
-      'zoom': 8
-    }
-  },
-  'location': {
-    'latitude': 52.35514938496378,
-    'longitude': 4.673877537499948,
-    'zoom': 8
-  },
-  'isFavorite': false,
-  'isPremium': false,
-  'rating': 4,
-  'description': 'A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam.',
-  'bedrooms': 3,
-  'goods': [
-    'Heating'
-  ],
-  'host': {
-    'name': 'Oliver Conner',
-    'avatarUrl': 'img/avatar-angelina.jpg',
-    'isPro': false
-  },
-  'images': [
-    'img/apartment-01.jpg'
-  ],
-  'maxAdults': 4,
-  previewImage: ''
-},
-{
-  'id': '8c870cc6-a80f-495c-b095-86e4f0c99510',
-  'title': 'Beautiful & luxurious studio at great location',
-  'type': 'apartment',
-  'price': 120,
-  'city': {
-    'name': 'Amsterdam',
-    'location': {
-      'latitude': 52.35514938496378,
-      'longitude': 4.673877537499948,
-      'zoom': 8
-    }
-  },
-  'location': {
-    'latitude': 52.35514938496378,
-    'longitude': 4.673877537499948,
-    'zoom': 8
-  },
-  'isFavorite': false,
-  'isPremium': false,
-  'rating': 4,
-  'description': 'A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam.',
-  'bedrooms': 3,
-  'goods': [
-    'Heating'
-  ],
-  'host': {
-    'name': 'Oliver Conner',
-    'avatarUrl': 'img/avatar-angelina.jpg',
-    'isPro': false
-  },
-  'images': [
-    'img/apartment-01.jpg'
-  ],
-  'maxAdults': 4,
-  previewImage: ''
-},
-{
-  'id': '13',
-  'title': 'Beautiful & luxurious studio at great location',
-  'type': 'apartment',
-  'price': 120,
-  'city': {
-    'name': 'Paris',
-    'location': {
-      'latitude': 52.35514938496378,
-      'longitude': 4.673877537499948,
-      'zoom': 8
-    }
-  },
-  'location': {
-    'latitude': 52.35514938496378,
-    'longitude': 4.673877537499948,
-    'zoom': 8
-  },
-  'isFavorite': true,
-  'isPremium': false,
-  'rating': 5,
-  'description': 'A quiet cozy and picturesque that hides behind a a river by the unique lightness of Paris.',
-  'bedrooms': 3,
-  'goods': [
-    'Heating, Towels'
-  ],
-  'host': {
-    'name': 'Oliver Conner',
-    'avatarUrl': 'img/avatar-angelina.jpg',
-    'isPro': true
-  },
-  'images': [
-    'img/apartment-01.jpg, img/apartment-03.jpg'
-  ],
-  'maxAdults': 4,
-  previewImage: ''
-},
-];
+// export const OFFERS: FullOffers = [{
+//   'id': 'fbb92630-ceb0-4ddf-a594-a4ae39ecaa91',
+//   'title': 'Beautiful & luxurious studio at great location',
+//   'type': 'apartment',
+//   'price': 120,
+//   'city': {
+//     'name': 'Paris',
+//     'location': {
+//       'latitude': 52.35514938496378,
+//       'longitude': 4.673877537499948,
+//       'zoom': 8
+//     }
+//   },
+//   'location': {
+//     'latitude': 52.35514938496378,
+//     'longitude': 4.673877537499948,
+//     'zoom': 8
+//   },
+//   'isFavorite': true,
+//   'isPremium': false,
+//   'rating': 5,
+//   'description': 'A quiet cozy and picturesque that hides behind a a river by the unique lightness of Paris.',
+//   'bedrooms': 3,
+//   'goods': [
+//     'Heating, Towels'
+//   ],
+//   'host': {
+//     'name': 'Oliver Conner',
+//     'avatarUrl': 'img/avatar-angelina.jpg',
+//     'isPro': true
+//   },
+//   'images': [
+//     'img/apartment-01.jpg, img/apartment-03.jpg'
+//   ],
+//   'maxAdults': 4,
+//   previewImage: ''
+// },
+// {
+//   'id': '2',
+//   'title': 'Beautiful & luxurious studio at great location',
+//   'type': 'apartment',
+//   'price': 120,
+//   'city': {
+//     'name': 'Paris',
+//     'location': {
+//       'latitude': 52.35514938496378,
+//       'longitude': 4.673877537499948,
+//       'zoom': 8
+//     }
+//   },
+//   'location': {
+//     'latitude': 52.35514938496378,
+//     'longitude': 4.673877537499948,
+//     'zoom': 8
+//   },
+//   'isFavorite': false,
+//   'isPremium': false,
+//   'rating': 4,
+//   'description': 'A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam.',
+//   'bedrooms': 3,
+//   'goods': [
+//     'Heating'
+//   ],
+//   'host': {
+//     'name': 'Oliver Conner',
+//     'avatarUrl': 'img/avatar-angelina.jpg',
+//     'isPro': false
+//   },
+//   'images': [
+//     'img/apartment-01.jpg'
+//   ],
+//   'maxAdults': 4,
+//   previewImage: ''
+// },
+// {
+//   'id': '8c870cc6-a80f-495c-b095-86e4f0c99510',
+//   'title': 'Beautiful & luxurious studio at great location',
+//   'type': 'apartment',
+//   'price': 120,
+//   'city': {
+//     'name': 'Amsterdam',
+//     'location': {
+//       'latitude': 52.35514938496378,
+//       'longitude': 4.673877537499948,
+//       'zoom': 8
+//     }
+//   },
+//   'location': {
+//     'latitude': 52.35514938496378,
+//     'longitude': 4.673877537499948,
+//     'zoom': 8
+//   },
+//   'isFavorite': false,
+//   'isPremium': false,
+//   'rating': 4,
+//   'description': 'A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam.',
+//   'bedrooms': 3,
+//   'goods': [
+//     'Heating'
+//   ],
+//   'host': {
+//     'name': 'Oliver Conner',
+//     'avatarUrl': 'img/avatar-angelina.jpg',
+//     'isPro': false
+//   },
+//   'images': [
+//     'img/apartment-01.jpg'
+//   ],
+//   'maxAdults': 4,
+//   previewImage: ''
+// },
+// {
+//   'id': '13',
+//   'title': 'Beautiful & luxurious studio at great location',
+//   'type': 'apartment',
+//   'price': 120,
+//   'city': {
+//     'name': 'Paris',
+//     'location': {
+//       'latitude': 52.35514938496378,
+//       'longitude': 4.673877537499948,
+//       'zoom': 8
+//     }
+//   },
+//   'location': {
+//     'latitude': 52.35514938496378,
+//     'longitude': 4.673877537499948,
+//     'zoom': 8
+//   },
+//   'isFavorite': true,
+//   'isPremium': false,
+//   'rating': 5,
+//   'description': 'A quiet cozy and picturesque that hides behind a a river by the unique lightness of Paris.',
+//   'bedrooms': 3,
+//   'goods': [
+//     'Heating, Towels'
+//   ],
+//   'host': {
+//     'name': 'Oliver Conner',
+//     'avatarUrl': 'img/avatar-angelina.jpg',
+//     'isPro': true
+//   },
+//   'images': [
+//     'img/apartment-01.jpg, img/apartment-03.jpg'
+//   ],
+//   'maxAdults': 4,
+//   previewImage: ''
+// },
+// ];
