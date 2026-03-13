7cd1c7218ca8ccbde480e1eb0f8a1555de93b594
Irina <i.ilchenko.spb@gmail.com>
2026-03-13 04:47:56 +0300

    useAppDispatch в useActionCreators

--- src/hooks/action-creators-hook.ts
+++ src/hooks/action-creators-hook.ts
@@ -1,6 +1,6 @@
 import { useMemo } from 'react';
-import { useDispatch } from 'react-redux';
 import { bindActionCreators, ActionCreatorsMapObject } from '@reduxjs/toolkit';
+import { useAppDispatch } from './store-hooks';
 
 export type BoundActions<T extends ActionCreatorsMapObject> = {
   [K in keyof T]: T[K] extends (...args: infer A) => infer R
@@ -11,7 +11,7 @@ export type BoundActions<T extends ActionCreatorsMapObject> = {
 export const useActionCreators = <T extends ActionCreatorsMapObject>(
   actions: T
 ): BoundActions<T> => {
-  const dispatch = useDispatch();
+  const dispatch = useAppDispatch();
 
   return useMemo(
     () => bindActionCreators(actions, dispatch) as unknown as BoundActions<T>,
