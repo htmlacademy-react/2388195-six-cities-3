6cb123bc8ae6223bb1c160f1ac6bf5d82f6892a3
Irina <i.ilchenko.spb@gmail.com>
2026-04-13 23:38:28 +0300

    карточка отеля - изменение ссылок

--- src/components/place-card.tsx
+++ src/components/place-card.tsx
@@ -34,23 +34,32 @@ export default function PlaceCard({
   hovered,
 }: PlaceCardProps): JSX.Element {
   const dispatch = useAppDispatch();
-  const { id, isPremium, previewImage, price, title, type, rating, isFavorite } = currentOffer;
+  const {
+    id,
+    isPremium,
+    previewImage,
+    price,
+    title,
+    type,
+    rating,
+    isFavorite,
+  } = currentOffer;
   const roundedRating = Math.round(rating);
   const starActiveWidth = getStarActiveWidth(roundedRating);
   const { width, height } = sizes[cardType];
 
   return (
-    <Link to={`${AppRoute.Offer}/${id}`}>
-      <article
-        className={`${cardType}__card place-card`}
-        onMouseEnter={() => hovered && dispatch(offersActions.setActiveId(id))}
-        onMouseLeave={() => hovered && dispatch(offersActions.setActiveId(null))}
-      >
-        {isPremium && (
-          <div className="place-card__mark">
-            <span>Premium</span>
-          </div>
-        )}
+    <article
+      className={`${cardType}__card place-card`}
+      onMouseEnter={() => hovered && dispatch(offersActions.setActiveId(id))}
+      onMouseLeave={() => hovered && dispatch(offersActions.setActiveId(null))}
+    >
+      {isPremium && (
+        <div className="place-card__mark">
+          <span>Premium</span>
+        </div>
+      )}
+      <Link to={`${AppRoute.Offer}/${id}`}>
         <div className={`${cardType}__image-wrapper place-card__image-wrapper`}>
           <img
             className="place-card__image"
@@ -60,30 +69,36 @@ export default function PlaceCard({
             alt="Place image"
           />
         </div>
-        <div
-          className={classNames(
-            'place-card__info',
-            cardType === 'favorites' && 'favorites__card-info',
-          )}
-        >
-          <div className="place-card__price-wrapper">
-            <div className="place-card__price">
-              <b className="place-card__price-value">&euro;{price}</b>
-              <span className="place-card__price-text">&#47;&nbsp;night</span>
-            </div>
-            <FavoriteButton buttonType={'place-card'} offerId={id} isFavorite={isFavorite} />
+      </Link>
+      <div
+        className={classNames(
+          'place-card__info',
+          cardType === 'favorites' && 'favorites__card-info',
+        )}
+      >
+        <div className="place-card__price-wrapper">
+          <div className="place-card__price">
+            <b className="place-card__price-value">&euro;{price}</b>
+            <span className="place-card__price-text">&#47;&nbsp;night</span>
           </div>
-          <div className="place-card__rating rating">
-            <div className="place-card__stars rating__stars">
-              <span style={{ width: starActiveWidth }}></span>
-              <span className="visually-hidden">Rating</span>
-            </div>
-          </div>
-          <h2 className="place-card__name">{title}</h2>
-          <p className="place-card__type">{formatedType(type)}</p>
+          <FavoriteButton
+            buttonType={'place-card'}
+            offerId={id}
+            isFavorite={isFavorite}
+          />
         </div>
-      </article>
-    </Link>
+        <div className="place-card__rating rating">
+          <div className="place-card__stars rating__stars">
+            <span style={{ width: starActiveWidth }}></span>
+            <span className="visually-hidden">Rating</span>
+          </div>
+        </div>
+        <Link to={`${AppRoute.Offer}/${id}`}>
+          <h2 className="place-card__name">{title}</h2>
+        </Link>
+        <p className="place-card__type">{formatedType(type)}</p>
+      </div>
+    </article>
   );
 }
 
